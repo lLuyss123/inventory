907ca636e7518592f768c41693f7425992fe2353
Luis Mejia <lmejiaidep@gmail.com>
2026-03-11 11:48:51 -0500

    Cambiando comillas dobles a comilla ssimples al mostrar variables en archivo funciones.py

--- functions.py
+++ functions.py
@@ -5,10 +5,10 @@ def add_to_inventory(value,inventory):
     
 def print_inventory(inventory):
     for i in range(len(inventory["items_name"])):
-        print(f"Product: {inventory["items_name"][i]}")
-        print(f"Unit Price: {inventory["prices_by_1"][i]}")
-        print(f"Quantity: {inventory["quantitys"][i]}")
-        print(f"Total: {inventory["total_price"][i]}")
+        print(f"Product: {inventory['items_name'][i]}")
+        print(f"Unit Price: {inventory['prices_by_1'][i]}")
+        print(f"Quantity: {inventory['quantitys'][i]}")
+        print(f"Total: {inventory['total_price'][i]}")
         print("")
         
 def total_pay(value1,value2):
